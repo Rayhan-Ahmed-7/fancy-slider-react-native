1360c0c26f7b5d40f064f0f434e0987ba8f8e96e
rayhan.ahmed <rayhan.ahmed@ibos.io>
2024-11-07 14:30:49 +0600

    :zap: pexels api added

--- components/PexelsWallpapers.tsx
+++ components/PexelsWallpapers.tsx
@@ -1,7 +1,23 @@
-import {View} from 'react-native';
-import {Text} from 'react-native-reanimated/lib/typescript/Animated';
+import {useQuery} from '@tanstack/react-query';
+import {Text, View} from 'react-native';
 
 export function PexelsWallpapers() {
+  const apiKey = 't3dvevS1g2wkO09x1tMILyxgSCDM8LzLaTlPQoUDdQgdd2pKiFnZPvsD';
+  const {data, error, isLoading, isError} = useQuery({
+    queryKey: ['wallpapers'],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://api.pexels.com/v1/search?query=mobile wallpaper&orientation=portrait&per_page=20`,
+        {
+          headers: {
+            Authorization: `${apiKey}`,
+          },
+        },
+      );
+      return response.json();
+    },
+  });
+  console.log(data);
   return (
     <View>
       <Text>Wallpapers</Text>
